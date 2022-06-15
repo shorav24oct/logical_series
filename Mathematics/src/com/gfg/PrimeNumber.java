487c7c89ff5fdd60d8317a9407f130316dcb5f00
shoravs <sharmasaurav142@gmail.com>
2022-06-15 21:26:46 +0530

    added navie solution

--- Mathematics/src/com/gfg/PrimeNumber.java
+++ Mathematics/src/com/gfg/PrimeNumber.java
@@ -2,9 +2,23 @@ package com.gfg;
 
 public class PrimeNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/*
+	 * naive solution
+	 * time complexity Big-O(n)
+	 */
+	public static boolean isPrime(int no) {
+		if (no == 1)
+			return false;
 
+		for (int i = 2; i < no; i++)
+			if (no % i == 0)
+				return false;
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Number is prime or not: " + isPrime(3));
 	}
 
 }
