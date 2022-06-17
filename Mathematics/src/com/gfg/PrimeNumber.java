0779cad79e62f3353061656ca808c8b128dd2e64
shoravs <sharmasaurav142@gmail.com>
2022-06-17 21:19:07 +0530

    add more efficient method

--- Mathematics/src/com/gfg/PrimeNumber.java
+++ Mathematics/src/com/gfg/PrimeNumber.java
@@ -3,8 +3,7 @@ package com.gfg;
 public class PrimeNumber {
 
 	/*
-	 * naive solution
-	 * time complexity Big-O(n)
+	 * naive solution time complexity Big-O(n)
 	 */
 	public static boolean isPrime(int no) {
 		if (no == 1)
@@ -17,8 +16,29 @@ public class PrimeNumber {
 		return true;
 	}
 
+	/*
+	 * more efficient method
+	 */
+	public static boolean isPrimeOrNot(int no) {
+		if (no == 1) 
+			return false;
+
+		if (no == 2 || no == 3)
+			return true;
+		
+		if (no % 2 == 0 || no % 3 == 0)
+			return false;
+		
+		for (int i = 5; i * i <= no; i = i + 6)
+			if (no % i == 0 || no % (i+2) == 0)
+				return false;
+		
+		return true;
+
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Number is prime or not: " + isPrime(3));
+		System.out.println("Number is prime or not: " + isPrimeOrNot(32));
 	}
 
 }
