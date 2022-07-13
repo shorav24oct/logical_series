b573eecaae61ac106ddb579be4958b5a2072d697
shoravs <sharmasaurav142@gmail.com>
2022-07-13 07:00:26 +0530

    add efficient and more efficient solution

--- Mathematics/src/com/gfg/PrimeNumber.java
+++ Mathematics/src/com/gfg/PrimeNumber.java
@@ -2,6 +2,8 @@ package com.gfg;
 
 public class PrimeNumber {
 
+	static int count = 0;
+
 	/*
 	 * naive solution time complexity Big-O(n)
 	 */
@@ -9,36 +11,57 @@ public class PrimeNumber {
 		if (no == 1)
 			return false;
 
-		for (int i = 2; i < no; i++)
+		for (int i = 2; i < no; i++) {
+			count++;
 			if (no % i == 0)
 				return false;
+		}
 
 		return true;
 	}
 
 	/*
-	 * more efficient method
+	 * efficient solution time complexity Big-O(/``n)
+	 */
+	public static boolean isPrime2(int no) {
+		if (no == 1)
+			return false;
+
+		for (int i = 2; i * i <= no; i++) {
+			count++;
+			if (no % i == 0)
+				return false;
+		}
+
+		return true;
+	}
+
+	/*
+	 * more efficient solution
 	 */
 	public static boolean isPrimeOrNot(int no) {
-		if (no == 1) 
+		if (no == 1)
 			return false;
 
 		if (no == 2 || no == 3)
 			return true;
-		
+
 		if (no % 2 == 0 || no % 3 == 0)
 			return false;
-		
-		for (int i = 5; i * i <= no; i = i + 6)
-			if (no % i == 0 || no % (i+2) == 0)
+
+		for (int i = 5; i * i <= no; i = i + 6) {
+			count++;
+			if (no % i == 0 || no % (i + 2) == 0)
 				return false;
-		
+		}
+
 		return true;
 
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Number is prime or not: " + isPrimeOrNot(32));
+		System.out.println("Number is prime or not: " + isPrimeOrNot(1031));
+		System.out.println(count);
 	}
 
 }
