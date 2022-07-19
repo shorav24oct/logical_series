31da30f064edc4acdd11e05eaf29f461da7dbd1d
shoravs <sharmasaurav142@gmail.com>
2022-07-19 07:23:46 +0530

    remove duplicates from sorted array

--- Arrays/src/com/gfg/array/RemoveDuplicates.java
+++ Arrays/src/com/gfg/array/RemoveDuplicates.java
@@ -5,9 +5,7 @@ import java.util.Arrays;
 public class RemoveDuplicates {
 
 	/*
-	 * Naive Solution
-	 * Time Complexity Big-O(n)
-	 * Space Complexity Big-O(n)
+	 * Naive Solution Time Complexity Big-O(n) Space Complexity Big-O(n)
 	 */
 	public static void remDuplicates(int arr[]) {
 		int temp[] = new int[arr.length];
@@ -24,20 +22,25 @@ public class RemoveDuplicates {
 			arr[i] = temp[i];
 		System.out.println(Arrays.toString(arr));
 	}
-	
+
 	/*
-	 * efficient solution
-	 * Time Complexity Big-O(n)
-	 * Space Complexity Big-O(1)
+	 * efficient solution Time Complexity Big-O(n) Space Complexity Big-O(1)
 	 */
-	public static void remove Duplicates(int[] arr) {
-		
+	public static void removeDuplicates(int[] arr) {
+		int res = 1;
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] != arr[res - 1]) {
+				arr[res] = arr[i];
+				res++;
+			}
+		}
+		System.out.println(Arrays.toString(arr));
 	}
 
 	public static void main(String[] args) {
 		int arr[] = { 10, 20, 20, 30, 30, 30, 30 };
 		System.out.println(Arrays.toString(arr));
-		remDuplicates(arr);
+		removeDuplicates(arr);
 	}
 
 }
