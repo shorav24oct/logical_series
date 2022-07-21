4ae292a13e2183b31ebf76548e66946b0a5be298
shoravs <sharmasaurav142@gmail.com>
2022-07-21 18:19:26 +0530

    find index of first occurrence

---
+++ Searching/src/com/gfg/search/IndexOfFirstOccurrence.java
@@ -0,0 +1,33 @@
+package com.gfg.search;
+
+public class IndexOfFirstOccurrence {
+
+	public static int search(int[] arr, int x) {
+		int low = 0;
+		int high = arr.length - 1;
+
+		while (low <= high) {
+			int mid = (low + high) / 2;
+
+			if (arr[mid] > x)
+				high = mid - 1;
+
+			else if (arr[mid] < x)
+				low = mid + 1;
+
+			else {
+				if (mid == 0 || arr[mid - 1] != arr[mid])
+					return mid;
+				else
+					high = mid - 1;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 20, 20, 20 };
+		System.out.println(search(arr, 20));
+	}
+
+}
