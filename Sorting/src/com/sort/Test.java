482c8a491d4b6ecd387229b0eb7652cccb35fdb7
shoravs <sharmasaurav142@gmail.com>
2022-07-27 07:57:53 +0530

    sorting by using comparator and comparable

---
+++ Sorting/src/com/sort/Test.java
@@ -0,0 +1,37 @@
+package com.sort;
+
+import java.util.Arrays;
+
+class Point implements Comparable<Point> {
+
+	int x;
+	int y;
+
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int compareTo(Point o) {
+		return this.x - o.x;
+	}
+
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		Point[] arr = { new Point(10, 20), new Point(5, 25), new Point(8, 80), new Point(1, 10) };
+		
+		for (int i = 0; i < arr.length; i++)
+			System.out.print("(" + arr[i].x + "," + arr[i].y + ") ");
+		System.out.println();
+		
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < arr.length; i++)
+			System.out.print("(" + arr[i].x + "," + arr[i].y + ") ");
+	}
+
+}
---
+++ Sorting/src/com/sort/TestGFG.java
@@ -0,0 +1,31 @@
+package com.sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class MyComparator implements Comparator<Point> {
+
+	@Override
+	public int compare(Point o1, Point o2) {
+		return o1.x - o2.y;
+	}
+
+}
+
+public class TestGFG {
+
+	public static void main(String[] args) {
+		Point[] arr = { new Point(10, 20), new Point(5, 25), new Point(8, 80), new Point(1, 10) };
+
+		for (int i = 0; i < arr.length; i++)
+			System.out.print("(" + arr[i].x + "," + arr[i].y + ") ");
+		System.out.println();
+
+		Arrays.sort(arr, new MyComparator());
+
+		for (int i = 0; i < arr.length; i++)
+			System.out.print("(" + arr[i].x + "," + arr[i].y + ") ");
+
+	}
+
+}
